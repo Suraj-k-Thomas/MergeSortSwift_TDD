ec0c93644b387b6bf2f8310c9db6307aa0ca155f
surajkthomas <ksurajt@gmail.com>
2025-03-08 13:09:22 +0530

    Initial Commit

---
+++ MergeSortSwift-TDD/MergeSortSwift_TDD.h
@@ -0,0 +1,18 @@
+//
+//  MergeSortSwift_TDD.h
+//  MergeSortSwift-TDD
+//
+//  Created by Suraj  Thomas on 08/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MergeSortSwift_TDD.
+FOUNDATION_EXPORT double MergeSortSwift_TDDVersionNumber;
+
+//! Project version string for MergeSortSwift_TDD.
+FOUNDATION_EXPORT const unsigned char MergeSortSwift_TDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MergeSortSwift_TDD/PublicHeader.h>
+
+
---
+++ MergeSortSwift-TDDTests/MergeSortSwift_TDDTests.swift
@@ -0,0 +1,17 @@
+//
+//  MergeSortSwift_TDDTests.swift
+//  MergeSortSwift-TDDTests
+//
+//  Created by Suraj  Thomas on 08/03/25.
+//
+
+import Testing
+@testable import MergeSortSwift_TDD
+
+struct MergeSortSwift_TDDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
