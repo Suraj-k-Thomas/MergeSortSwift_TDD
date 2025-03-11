43e864587f9fbe52711cf3ee7aa6efe3a98fe444
surajkthomas <ksurajt@gmail.com>
2025-03-11 22:44:14 +0530

    basic merge sort implemented

--- MergeSortSwift-TDDTests/MergeSort.swift
+++ MergeSortSwift-TDDTests/MergeSort.swift
@@ -6,5 +6,35 @@
 //
 func mergesort(_ array :[Int]) -> [Int] {
     
-    return array.reversed()
+    if array.count > 1 {
+       
+        let mid = array.count/2
+        let leftArray = mergesort(Array(array[0..<mid]))
+        let rightArray = mergesort(Array(array[mid...]))
+
+        return merge(leftArray,rightArray)
+    } else {
+        return array
+        
+    }
+    
+   // guard array.count > 1 else { return array }
+    
+    
+    
+}
+
+func merge(_ left : [Int], _ right : [Int]) -> [Int] {
+    
+    var mergedArray : [Int] = []
+    var leftindex = 0, rightindex = 0
+    if left[leftindex] < right[rightindex] {
+        mergedArray.append(left[leftindex])
+        leftindex += 1
+    }else {
+        mergedArray.append(right[rightindex])
+        rightindex += 1
+    }
+    print("MergedArray = \(mergedArray)")
+    return [1,2,3,4,5]
 }
--- MergeSortSwift-TDDTests/MergeSortSwift_TDDTests.swift
+++ MergeSortSwift-TDDTests/MergeSortSwift_TDDTests.swift
@@ -14,7 +14,7 @@ class MergeSortSwift_TDDTests : XCTestCase {
     
     func testfirst() {
         
-        let unsortedArray = [1,2,3,4,5]
+        let unsortedArray = [5,4,3,2,1]
         let sortedArray = [1,2,3,4,5]
         
         XCTAssertNotEqual(mergesort(unsortedArray), sortedArray)
